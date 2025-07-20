213795444f350322ddedc511c42e392980697931
Viveik29 <viveik29@gmail.com>
2025-07-20 13:34:09 +0530

    Updated unit test file

--- test_app.py
+++ test_app.py
@@ -1,7 +1,8 @@
 from app import app
 
 def test_home():
-    response = app.test_client.get('/')
+    tester = app.test_client()  # instantiate test client
+    response = tester.get('/')  # make GET request
 
-    assert response.status_code == 200 
+    assert response.status_code == 200
     assert response.data == b'Hello world!'
